73982f8131b47edde8e184ad565c85a232615eb2
whozek <khuzairirossi12@gmail.com>
2026-02-17 13:05:18 +0800

    ADD PRODUCT

--- toko-game-kita/public/product.js
+++ toko-game-kita/public/product.js
@@ -1,33 +1,93 @@
 const products = [
     { 
         id: 1, 
-        name: "Harvester Gun", 
-        price: 35.50, 
+        name: "VIP + LUCK", 
+        price: 10.35, 
         oldPrice: 45.00, 
         disc: "21%", 
         img: "https://placehold.co/300x300/1a1b3a/white?text=Gun",
         stock: "In Stock",
-        category: "MM2" // Tambah ini
+        category: "FISH IT" // Tambah ini
     },
     { 
         id: 2, 
-        name: "Candy Cane Knife", 
-        price: 28.00, 
+        name: "MUTATIONS", 
+        price: 6.90, 
         oldPrice: 32.00, 
         disc: "12%", 
         img: "https://placehold.co/300x300/1a1b3a/white?text=Knife",
         stock: "In Stock",
-        category: "MM2"
+        category: "FISH IT"
     },
     { 
         id: 3, 
-        name: "Huge Cat", 
-        price: 150.00, 
+        name: "ADVANCED LUCK", 
+        price: 12.65, 
         oldPrice: 200.00, 
         disc: "25%", 
         img: "https://placehold.co/300x300/1a1b3a/white?text=Pet",
         stock: "In Stock",
-        category: "PET SIM"
+        category: "FISH IT"
+    }
+    { 
+        id: 4, 
+        name: "EXTRA LUCK", 
+        price: 5.75, 
+        oldPrice: 200.00, 
+        disc: "25%", 
+        img: "https://placehold.co/300x300/1a1b3a/white?text=Pet",
+        stock: "In Stock",
+        category: "FISH IT"
+    }
+    { 
+        id: 5, 
+        name: "SMALL LUCK", 
+        price: 1.20, 
+        oldPrice: 200.00, 
+        disc: "25%", 
+        img: "https://placehold.co/300x300/1a1b3a/white?text=Pet",
+        stock: "In Stock",
+        category: "FISH IT"
+    }
+    { 
+        id: 6, 
+        name: "DOUBLE EXP", 
+        price: 4.60, 
+        oldPrice: 200.00, 
+        disc: "25%", 
+        img: "https://placehold.co/300x300/1a1b3a/white?text=Pet",
+        stock: "In Stock",
+        category: "FISH IT"
+    }
+    { 
+        id: 7, 
+        name: "MINI HOVERBOAT", 
+        price: 5.29, 
+        oldPrice: 200.00, 
+        disc: "25%", 
+        img: "https://placehold.co/300x300/1a1b3a/white?text=Pet",
+        stock: "In Stock",
+        category: "FISH IT"
+    }FISH IT
+    { 
+        id: 8, 
+        name: "SELL ANYWHERE", 
+        price: 7.36, 
+        oldPrice: 200.00, 
+        disc: "25%", 
+        img: "https://placehold.co/300x300/1a1b3a/white?text=Pet",
+        stock: "In Stock",
+        category: "FISH IT"
+    }
+    { 
+        id: 9, 
+        name: "HYPER BOAT PACK", 
+        price: 23, 
+        oldPrice: 200.00, 
+        disc: "25%", 
+        img: "https://placehold.co/300x300/1a1b3a/white?text=Pet",
+        stock: "In Stock",
+        category: "FISH IT"
     }
 ];
 
