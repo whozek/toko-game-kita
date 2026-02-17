0a06a938761d5af2d5cd9bf2dd5a019fe1aa3b10
whozek <khuzairirossi12@gmail.com>
2026-02-17 12:21:56 +0800

    Tambah menu kategori

--- toko-game-kita/public/product.js
+++ toko-game-kita/public/product.js
@@ -6,7 +6,8 @@ const products = [
         oldPrice: 45.00, 
         disc: "21%", 
         img: "https://placehold.co/300x300/1a1b3a/white?text=Gun",
-        stock: "In Stock"
+        stock: "In Stock",
+        category: "MM2" // Tambah ini
     },
     { 
         id: 2, 
@@ -15,27 +16,19 @@ const products = [
         oldPrice: 32.00, 
         disc: "12%", 
         img: "https://placehold.co/300x300/1a1b3a/white?text=Knife",
-        stock: "Low Stock"
+        stock: "In Stock",
+        category: "MM2"
     },
     { 
         id: 3, 
-        name: "Heartblade Knife", 
-        price: 50.00, 
-        oldPrice: 65.00, 
-        disc: "23%", 
-        img: "https://placehold.co/300x300/1a1b3a/white?text=Heart",
-        stock: "In Stock"
-    },
-    { 
-        id: 4, 
-        name: "Luger Gun", 
-        price: 42.00, 
-        oldPrice: 50.00, 
-        disc: "16%", 
-        img: "https://placehold.co/300x300/1a1b3a/white?text=Luger",
-        stock: "Out of Stock"
+        name: "Huge Cat", 
+        price: 150.00, 
+        oldPrice: 200.00, 
+        disc: "25%", 
+        img: "https://placehold.co/300x300/1a1b3a/white?text=Pet",
+        stock: "In Stock",
+        category: "PET SIM"
     }
 ];
 
-// Supaya file lain boleh baca data ini
 window.gameProducts = products;
