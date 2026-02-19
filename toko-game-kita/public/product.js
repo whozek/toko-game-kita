2d508021dfb8cf09e983278da5ec2043c73da5a3
whozek <khuzairirossi12@gmail.com>
2026-02-19 15:28:15 +0800

    Fix

--- toko-game-kita/public/product.js
+++ toko-game-kita/public/product.js
@@ -64,7 +64,8 @@ const allProducts = [
         img: "https://cdn.discordapp.com/attachments/1154147488341704745/1473521420158898227/IMG_2575.jpg?ex=69968343&is=699531c3&hm=8fe86f695255badf40127d442c16da411fa0fbc2eec74698fedb548ba2420d99&",
         isVariant: false,
         type: "Digital",
-        isRate: false
+        isRate: false,
+        allowQuantity: true
     }
 ];
 
